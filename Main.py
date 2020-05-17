e29d2a8ab2f260beee8eee9f4135a41fc2c93bfc
Casper Andersson <andersson.casper99@gmail.com>
2020-05-17 10:05:25 +0200

    Wrote the pruning algorithm from Youtube.

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 
+import random
 
 class Game:
   def __init__(self, size):
@@ -12,13 +13,85 @@ class Game:
         self.board[i].append(' ')
 
   def printBoard(self):
+    num = self.size
     for i in range(self.size):
+      print(" ", end="")
+    for i in range(self.size):
+      print("/\\", end="")
+    print()
+
+    for i in range(self.size):
+      num = num - 1
+      for _ in range(num):
+        print(" ", end="")
+
+      print("|", end="")
       for j in range(self.size):
-        print(self.board[i][j], end="")
+        print(str(self.board[i][j]) + "|", end="")
       print()
 
+      for _ in range(num):
+        print(" ", end="")
+      print("/", end="")
+      for _ in range(self.size):
+        print("\\/", end="")
+      print()
+      # print("\n---------------")
 
-  def minimax(self, x, y, boardcopy):
+
+  
+#   /\
+#  |5 |
+#   \/
+
+
+  def staticEvaluation(self, x, y):
+    # Counts the score for the position.
+    # if x == 2 and y == 2:
+    #   return -5
+    # if player == 'X':
+    #   return random.randint(-5,5)
+    # return 0
+    return random.randrange(-5, 6)
+
+  def minimax(self, x, y, depth, alpha, beta, player):
+    if (depth == 0): # Or game over
+      return self.staticEvaluation(x, y)
+    
+    if player == 'X':
+      maxEval = -999
+      for i in range(self.size):
+        for j in range(self.size):
+          if (self.board[i][j] == ' '):
+            self.board[i][j] = 'X'
+            value = self.minimax(i, j, depth - 1, alpha, beta, 'O')
+            print(value)
+            self.board[i][j] = ' '
+            maxEval = max(maxEval, value)
+            alpha = max(alpha, value)
+            if beta <= alpha:
+              # print("Pruning X")
+              break
+      
+      return maxEval
+            
+    else: 
+      minEval = 999
+      for i in range(self.size):
+        for j in range(self.size):
+          if (self.board[i][j] == ' '):
+            self.board[i][j] = 'O'
+            value = self.minimax(i, j, depth - 1, alpha, beta, 'X')
+            print(value)
+            self.board[i][j] = ' '
+            minEval = min(minEval, value)
+            beta = min(beta, value)
+            if beta <= alpha:
+              # print("Pruning O")
+              break
+      
+      return minEval
+
 
     # Undo any changes on board.
     return 0
@@ -28,16 +101,46 @@ class Game:
       # valueboard = self.board.copy()
       bestI = 0
       bestJ = 0
-      maxValue = 0
+      maxValue = -999
       for i in range(self.size):
         for j in range(self.size):
-          newMax = max(maxValue, self.minimax(i, j, self.board))
-          if (newMax > maxValue):
-            bestI = i
-            bestJ = j
-      pass
+          # Replace 0, 0 with alpha-beta values
+          if self.board[i][j] == ' ':
+            newMax = max(maxValue, self.minimax(i, j, 3, -999, 999, self.turn)) 
+            # print("Newmax: " + str(newMax))
+            # print("maxValue: " + str(maxValue))
+            if (newMax > maxValue):
+              bestI = i
+              bestJ = j
+              maxValue = newMax
+      
+      print("Placing X in position " + str(bestI) + ":" + str(bestJ))
+      self.board[bestI][bestJ] = "X"
+      self.turn = 'O'
+        
     elif (self.turn == "O"):
-      pass
+      bestI = 0
+      bestJ = 0
+      minValue = 999
+      for i in range(self.size):
+        for j in range(self.size):
+          # Replace 0, 0 with alpha-beta values
+          if self.board[i][j] == ' ':
+            newMin = min(minValue, self.minimax(i, j, 3, -999, 999, self.turn)) 
+            if (newMin < minValue):
+              bestI = i
+              bestJ = j
+              minValue = newMin
+      
+      print("Placing O in position " + str(bestI) + ":" + str(bestJ))
+      self.board[bestI][bestJ] = "O"
+      self.turn = 'X'
+
+  def botPlay(self):
+    for _ in range(4):
+      self.doBestMove()
+    
+    self.printBoard()
 
 
   def playGame(self):
@@ -60,9 +163,20 @@ class Game:
 
 
 
-game = Game(5)
+game = Game(2)
 game.printBoard()
 
-game.playGame()
+game.botPlay()
+# game.playGame()
+
+
+
+
+
+
+
+
+
+
 
 
