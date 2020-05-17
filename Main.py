d32e518a14697738cd55cc4b6fb4ed176b4c7e74
Casper Andersson <andersson.casper99@gmail.com>
2020-05-17 11:45:59 +0200

    Some stuff for static evaluation

--- Main.py
+++ Main.py
@@ -45,19 +45,46 @@ class Game:
 #  |5 |
 #   \/
 
+  def getNeighbours(self, x, y):
+    ls = [(x+1,y), (x-1, y), (x, y+1), (x, y-1), (x-1, y-1), (x+1, y+1)]
+    newls = []
+    for (i,j) in ls:
+      if i >= 0 and i < self.size and j >= 0 and j < self.size:
+        newls.append((i,j))
+    
+    return newls
 
-  def staticEvaluation(self, x, y):
+  def getDiagonals(self, x, y):
+    # ls = [(x-2, y-1), (x-2, y+1), (x+2, y-1), (x+2, y+1)]
+    ls = [(x-1, y-2), (x-2, y-1), (x-1,y+1), (x+1, y+2), (x+2, y+1), (x+1, y-1)]
+    newls = []
+    for (i,j) in ls:
+      if i >= 0 and i < self.size and j >= 0 and j < self.size:
+        newls.append((i,j))
+    
+    return newls
+
+  def staticEvaluation(self, x, y, player):
+    notPlayer = 'X' if player == 'O' else 'O'
+    val = 0
+    for (i,j) in self.getNeighbours(x, y):
+      if self.board[i][j] == player:
+        val = val + 1
+      elif self.board[i][j] == notPlayer:
+        val = val - 2
+
+    return val if player == 'X' else -val
     # Counts the score for the position.
     # if x == 2 and y == 2:
     #   return -5
     # if player == 'X':
     #   return random.randint(-5,5)
     # return 0
-    return random.randrange(-5, 6)
+    # return random.randrange(-5, 6)
 
   def minimax(self, x, y, depth, alpha, beta, player):
     if (depth == 0 or self.moves == self.size*self.size): # Or game over
-      return self.staticEvaluation(x, y)
+      return self.staticEvaluation(x, y, player)
     
     if player == 'X':
       maxEval = -999
@@ -67,7 +94,9 @@ class Game:
             self.moves = self.moves + 1
             self.board[i][j] = 'X'
             value = self.minimax(i, j, depth - 1, alpha, beta, 'O')
-            print(value)
+            # print(value)
+            # print("Depth: " + str(depth) + "| X: " + str(value))
+            # print("Checking position: " + str(i) + " " + str(j))
             self.board[i][j] = ' '
             self.moves = self.moves - 1
             maxEval = max(maxEval, value)
@@ -86,7 +115,9 @@ class Game:
             self.board[i][j] = 'O'
             self.moves = self.moves + 1
             value = self.minimax(i, j, depth - 1, alpha, beta, 'X')
-            print(value)
+            # print(value)
+            # print("X: " + str(i) + " | Y: " + str(j) + " | " + str(value))
+            # print("Depth: " + str(depth) + " | O: " + str(value))
             self.board[i][j] = ' '
             self.moves = self.moves - 1
             minEval = min(minEval, value)
@@ -111,7 +142,7 @@ class Game:
         for j in range(self.size):
           # Replace 0, 0 with alpha-beta values
           if self.board[i][j] == ' ':
-            newMax = max(maxValue, self.minimax(i, j, 3, -999, 999, self.turn)) 
+            newMax = max(maxValue, self.minimax(i, j, 0, -999, 999, self.turn)) 
             # print("Newmax: " + str(newMax))
             # print("maxValue: " + str(maxValue))
             if (newMax > maxValue):
@@ -132,11 +163,14 @@ class Game:
         for j in range(self.size):
           # Replace 0, 0 with alpha-beta values
           if self.board[i][j] == ' ':
-            newMin = min(minValue, self.minimax(i, j, 3, -999, 999, self.turn)) 
+            newMin = min(minValue, self.minimax(i, j, 1, -999, 999, self.turn)) 
             if (newMin < minValue):
               bestI = i
               bestJ = j
               minValue = newMin
+              print(newMin)
+              print("BestI: " + str(bestI))
+              print("BestJ: " + str(bestJ))
       
       print("Placing O in position " + str(bestI) + ":" + str(bestJ))
       self.board[bestI][bestJ] = "O"
@@ -144,7 +178,7 @@ class Game:
       self.moves = self.moves + 1
 
   def botPlay(self):
-    for _ in range(4):
+    for _ in range(25):
       self.doBestMove()
     
     self.printBoard()
